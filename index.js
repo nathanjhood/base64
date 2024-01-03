b304fdf99509a7fd9c2d00413d2b670cd4005c35
StoneyDSP <nathanjhood@googlemail.com>
2024-01-03 12:17:27 +0100

    Added some more NodeJS module tests
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import base64 from './base64.cjs'
 console.log(base64.hello());
 console.log(base64.version());
 
+console.log("\nTest vector:\n");
+
 // test vector
 console.log("");
 console.log("f");
@@ -13,6 +15,8 @@ console.log("foob");
 console.log("fooba");
 console.log("foobar");
 console.log("");
+
+console.log("\nEncoder test:\n");
 
 // encoder test
 console.log(base64.encode(""));
@@ -24,12 +28,27 @@ console.log(base64.encode("fooba"));
 console.log(base64.encode("foobar"));
 console.log(base64.encode(""));
 
+console.log("\nDecoder test:\n");
+
 // decoder test
-// console.log(base64.decode(""));
-console.log(base64.decode("Zg=="));
-console.log(base64.decode("Zm8="));
-console.log(base64.decode("Zm9v"));
-console.log(base64.decode("Zm9vYg=="));
-console.log(base64.decode("Zm9vYmE="));
-console.log(base64.decode("Zm9vYmFy"));
-// console.log(base64.decode(""));
+console.log(base64.decode("\n"));
+console.log(base64.decode("Zg==\n"));
+console.log(base64.decode("Zm8=\n"));
+console.log(base64.decode("Zm9v\n"));
+console.log(base64.decode("Zm9vYg==\n"));
+console.log(base64.decode("Zm9vYmE=\n"));
+console.log(base64.decode("Zm9vYmFy\n"));
+console.log(base64.decode("\n"));
+
+console.log("\nEncoder URL alphabet test:\n");
+
+// encoder mode test
+console.log(base64.encode("=", false));
+console.log(base64.encode("/", false));
+console.log(base64.encode("-", false));
+console.log(base64.encode("_ ", false));
+
+console.log(base64.encode("=", true));
+console.log(base64.encode("/", true));
+console.log(base64.encode("-", true));
+console.log(base64.encode("_", true));
