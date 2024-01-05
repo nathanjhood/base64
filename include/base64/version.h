f2bd439c1f79be28e559b6a45b8a656081187efd
StoneyDSP <nathanjhood@googlemail.com>
2024-01-05 06:37:26 +0100

    Version increment
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- include/base64/version.h
+++ include/base64/version.h
@@ -1,4 +1,4 @@
-// base64 v1.0.0.a005d2eae11830278fb0b853790eebe585f2c543
+// base64 v1.0.0.2da00e2796d5d949e71b97a5ac83fe8a2a955fb9
 // Version control info
 
 #ifndef base64_VERSION_H_
@@ -7,8 +7,8 @@
 #define base64_VERSION_MAJOR "1"
 #define base64_VERSION_MINOR "0"
 #define base64_VERSION_PATCH "0"
-#define base64_VERSION_TWEAK "a005d2eae11830278fb0b853790eebe585f2c543"
-#define base64_VERSION "1.0.0.a005d2eae11830278fb0b853790eebe585f2c543"
+#define base64_VERSION_TWEAK "2da00e2796d5d949e71b97a5ac83fe8a2a955fb9"
+#define base64_VERSION "1.0.0.2da00e2796d5d949e71b97a5ac83fe8a2a955fb9"
 #define base64_REPOSITORY "https://github.com/nathanjhood/base64.git"
 
 #endif // base64_VERSION_H_
