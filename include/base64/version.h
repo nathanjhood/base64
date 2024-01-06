0587f58a99131d936eb9bdd756f14151c9d8b602
StoneyDSP <nathanjhood@googlemail.com>
2024-01-07 00:59:41 +0100

    Enhanced comments for visitors
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- include/base64/version.h
+++ include/base64/version.h
@@ -1,4 +1,4 @@
-// base64 v1.0.0.75cb91213dd8213da89546bc8b9fd4e4e5ecd494
+// base64 v1.0.0.9d894cd0c3756d0638f12b43c59dd98bd3d6d5c8
 // Version control info
 
 #ifndef base64_VERSION_H_
@@ -7,8 +7,8 @@
 #define base64_VERSION_MAJOR "1"
 #define base64_VERSION_MINOR "0"
 #define base64_VERSION_PATCH "0"
-#define base64_VERSION_TWEAK "75cb91213dd8213da89546bc8b9fd4e4e5ecd494"
-#define base64_VERSION "1.0.0.75cb91213dd8213da89546bc8b9fd4e4e5ecd494"
+#define base64_VERSION_TWEAK "9d894cd0c3756d0638f12b43c59dd98bd3d6d5c8"
+#define base64_VERSION "1.0.0.9d894cd0c3756d0638f12b43c59dd98bd3d6d5c8"
 #define base64_REPOSITORY "https://github.com/nathanjhood/base64.git"
 
 #endif // base64_VERSION_H_
