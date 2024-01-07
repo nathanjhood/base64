34a76e92d28ea545e1eb63c36d291ee17e46abdc
StoneyDSP <nathanjhood@googlemail.com>
2024-01-07 05:34:49 +0100

    Improved Typescript support for Javascript Intellisense
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- include/base64/version.h
+++ include/base64/version.h
@@ -1,4 +1,4 @@
-// base64 v1.0.0.0517c1f5a8ef62f2fef3e3cd7541ad7b48fe7d56
+// base64 v1.0.0.e58f11305d51724ce76d7b925135423ffa8122e0
 // Version control info
 
 #ifndef base64_VERSION_H_
@@ -7,8 +7,8 @@
 #define base64_VERSION_MAJOR "1"
 #define base64_VERSION_MINOR "0"
 #define base64_VERSION_PATCH "0"
-#define base64_VERSION_TWEAK "0517c1f5a8ef62f2fef3e3cd7541ad7b48fe7d56"
-#define base64_VERSION "1.0.0.0517c1f5a8ef62f2fef3e3cd7541ad7b48fe7d56"
+#define base64_VERSION_TWEAK "e58f11305d51724ce76d7b925135423ffa8122e0"
+#define base64_VERSION "1.0.0.e58f11305d51724ce76d7b925135423ffa8122e0"
 #define base64_REPOSITORY "https://github.com/nathanjhood/base64.git"
 
 #endif // base64_VERSION_H_
--- lib/base64.node.ts
+++ lib/base64.node.ts
@@ -1,9 +1,37 @@
 interface base64 {
+  /**
+   * Returns a string, confirming the module is online.
+   * @returns string
+   */
   hello(): string;
+  /**
+   * Returns a string, confirming the module version number.
+   * @returns string
+   */
   version(): string;
+  /**
+   * Takes a string input, and returns a base64-encoded copy.
+   * @param string
+   */
   encode(string: string): string;
+  /**
+   * Takes a string input, and returns a base64-encoded copy. Also accepts a
+   * boolean, to specify if the input contains URL-like characters.
+   * @param string
+   * @param urlMode
+   */
   encode(string: string, urlMode: boolean): string;
+  /**
+   * Takes a base64-encoded string input, and returns a decoded copy.
+   * @param string
+   */
   decode(string: string): string;
+  /**
+   * Takes a base64-encoded string input, and returns a decoded copy. Also
+   * accepts a boolean, to specify if the input contains URL-like characters.
+   * @param string
+   * @param urlMode
+   */
   decode(string: string, urlMode: boolean): string;
 }
 const base64: base64 = require('../build/lib/base64.node');
